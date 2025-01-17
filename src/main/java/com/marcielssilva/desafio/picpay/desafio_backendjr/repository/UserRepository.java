f8e33116df606af835feaf0c322be9a6e0ded5dd
Marcielsilva <marciel.ssilva2@gmail.com>
2025-01-17 14:08:49 -0300

    Construção do package repository

---
+++ src/main/java/com/marcielssilva/desafio/picpay/desafio_backendjr/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.marcielssilva.desafio.picpay.desafio_backendjr.repository;
+
+import com.marcielssilva.desafio.picpay.desafio_backendjr.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByDocument(String document);
+}
